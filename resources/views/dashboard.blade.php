8da20235860ad5d4be992e393557e88b2fe26618
Luca Longo <l.longo@ambita.it>
2025-02-17 18:35:38 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
     <flux:heading size="xl" level="1">Dashboard</flux:heading>
 
-    <flux:subheading size="lg" class="mb-6">Good afternoon, {{ auth()->user()->name }}</flux:subheading>
+    <flux:subheading size="lg" class="mb-6">
+        Good afternoon, <span x-text="$store.currentUser.name"></span>
+    </flux:subheading>
 
     <flux:separator variant="subtle" />
 </x-app-layout>
