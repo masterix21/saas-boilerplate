cbc452e3151407a99cc8da719836385fecd86f6a
Luca Longo <l.longo@ambita.it>
2025-03-02 16:03:14 +0100

    wip

--- app/Http/Controllers/Subscriptions/BuyController.php
+++ app/Http/Controllers/Subscriptions/BuyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Subscriptions;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use LucaLongo\Subscriptions\Models\Plan;
 
 class BuyController extends Controller
@@ -10,16 +11,35 @@ class BuyController extends Controller
     public function __invoke(Plan $plan, string $gateway)
     {
         return match ($gateway) {
-            // 'stripe' => $this->stripeCheckout($plan),
+            'stripe' => $this->stripeCheckout($plan),
             // 'paypal' => $this->paypalCheckout($plan),
             // 'paddle' => $this->paddleCheckout($plan),
-            default => redirect()
-                ->route('app.subscribe')
-                ->withErrors(['payment' => __('Payment gateway not supported.') ]),
+            default => $this->gatewayNotSupportedError(),
         };
     }
 
-    protected function createStripePaymentIntent(Plan $plan)
+    protected function gatewayNotSupportedError(): RedirectResponse
     {
+        return redirect()
+            ->route('app.subscribe')
+            ->withErrors(['payment' => __('Payment gateway not supported.') ]);
+    }
+
+    protected function planUnsupportedByGateway(): RedirectResponse
+    {
+        return redirect()
+            ->route('app.subscribe')
+            ->withErrors(['payment' => __('Plan not supported by payment gateway.') ]);
+    }
+
+    protected function stripeCheckout(Plan $plan)
+    {
+        if (blank(config('services.stripe.key'))) {
+            return $this->gatewayNotSupportedError();
+        }
+
+        if (blank($plan->meta['stripe_id'] ?? null)) {
+            return $this->planUnsupportedByGateway();
+        }
     }
 }
